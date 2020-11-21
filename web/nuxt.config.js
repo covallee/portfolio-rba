b7abbd42050527aece026e32f49bbba02f43dde5
Constant V <constantvallee@gmail.com>
2020-11-21 10:11:08 -0500

    Update Favicon

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -49,8 +49,9 @@ export default {
       },
     ],
     link: [
-      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // { rel: 'icon', size: '16x16', href: '/favicon16x16.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', size: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', size: '16x16', href: '/favicon-32x32.png' },
     ],
   },
   /*
